3c3624f8478c643df2fbee6d61b8fd471d56c124
Atekkey <anand@tekkey.net>
2026-04-09 13:52:39 -0500

    launch edit

--- launcher.py
+++ launcher.py
@@ -1,7 +1,10 @@
 import subprocess
 import time
+import RPi.GPIO as GPIO
+
 
 while True:
+    GPIO.cleanup()
     subprocess.run(["python", "main.py"])
     print("main.py crashed, restarting in 2 seconds...")
     time.sleep(2)
