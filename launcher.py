820a191bd1f6fa8ed97704198c24d54a8f4f9ef2
Atekkey <anand@tekkey.net>
2026-04-09 13:54:18 -0500

    launch edit

--- launcher.py
+++ launcher.py
@@ -1,10 +1,11 @@
 import subprocess
+import sys
 import time
 import RPi.GPIO as GPIO
 
 
 while True:
     GPIO.cleanup()
-    subprocess.run(["python", "main.py"])
+    subprocess.run(["python", "main.py"] + sys.argv[1:])
     print("main.py crashed, restarting in 2 seconds...")
     time.sleep(2)
