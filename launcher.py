f1e84ac0f3c62901d0ff3b387e181105b43828d7
Atekkey <anand@tekkey.net>
2026-04-09 14:00:36 -0500

    edit

--- launcher.py
+++ launcher.py
@@ -3,10 +3,9 @@ import sys
 import time
 import RPi.GPIO as GPIO
 
-
+subprocess.run(["git", "pull"])
 while True:
     GPIO.cleanup()
-    subprocess.run(["git", "pull"])
     subprocess.run(["python", "main.py"] + sys.argv[1:])
     print("main.py crashed, restarting in 2 seconds...")
     time.sleep(2)
