497cd0de88509c2c022ceb7ef0f0391fb09b24c1
Atekkey <anand@tekkey.net>
2026-04-09 14:08:17 -0500

    edit

--- launcher.py
+++ launcher.py
@@ -5,7 +5,12 @@ import RPi.GPIO as GPIO
 
 subprocess.run(["git", "pull"])
 while True:
-    GPIO.cleanup()
-    subprocess.run(["python", "main.py"] + sys.argv[1:])
-    print("main.py crashed, restarting in 2 seconds...")
-    time.sleep(2)
+    try:
+        GPIO.cleanup()
+        subprocess.run(["python", "main.py"] + sys.argv[1:])
+        print("main.py crashed, restarting in 2 seconds...")
+        time.sleep(2)
+    except KeyboardInterrupt:
+        print("Stopped by user")
+        GPIO.cleanup()
+        break
